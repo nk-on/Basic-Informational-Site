d6fd257bc86fb015a71bec1f423b8e3ddba575b9
nk-on <2nkoz@proton.me>
2025-08-19 21:34:19 +0400

    Refactor:putting repeated code inside separate function

--- server.js
+++ server.js
@@ -3,25 +3,24 @@ const fs = require("node:fs");
 function writeHead(res, statusCode) {
   res.writeHead(statusCode, { "Content-type": "text/html" });
 }
-
+function sendHtml(res, filepath) {
+  const indexHtml = fs.readFileSync(filepath, res);
+  res.write(indexHtml);
+}
 const server = http.createServer((req, res) => {
   const url = req.url;
   if (url === "/") {
     writeHead(res, 200);
-    const indexHtml = fs.readFileSync("index.html", "utf-8");
-    res.write(indexHtml);
+    sendHtml(res, "index.html");
   } else if (url === "/about") {
     writeHead(res, 200);
-    const indexHtml = fs.readFileSync("about.html", "utf-8");
-    res.write(indexHtml);
+    sendHtml(res, "about.html");
   } else if (url === "/contact") {
     writeHead(res, 200);
-    const indexHtml = fs.readFileSync("contact-me.html", "utf-8");
-    res.write(indexHtml);
+    sendHtml(res, "contact-me.html");
   } else {
     writeHead(res, 404);
-    const indexHtml = fs.readFileSync("404.html", "utf-8");
-    res.write(indexHtml);
+    sendHtml(res, "404.html");
   }
   res.end();
 });
